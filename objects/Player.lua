6e9493a4f139b9a6e11968c9c40833951cfd16f5
mmdlow <mmdlow@gmail.com>
2020-07-30 00:56:44 +0800

    Add Fighter ship design

--- objects/Player.lua
+++ objects/Player.lua
@@ -14,14 +14,48 @@ function Player:new(area, x, y, opts)
   self.max_v = self.base_max_v  -- current max velocity
   self.a = 100                  -- player acceleration
   self.trail_color = skill_point_color
+  self.ship = 'Fighter'
+  self.polygons = {}
 
   self.timer:every(0.24, function() self:shoot() end)
   self.timer:every(5, function() self:tick() end)
   self.timer:every(0.01, function()
-    self.area:addGameObject('TrailParticle',
-    self.x - self.w * math.cos(self.r), self.y - self.h * math.sin(self.r),
-    {parent = self, r = random(2, 4), d = random(0.15, 0.25), color = self.trail_color})
+    if self.ship == 'Fighter' then
+      self.area:addGameObject('TrailParticle',
+      self.x - 0.9 * self.w * math.cos(self.r) + 0.2 * self.w * math.cos(self.r - math.pi / 2), 
+      self.y - 0.9 * self.w * math.sin(self.r) + 0.2 * self.w * math.sin(self.r - math.pi / 2), 
+      {parent = self, r = random(2, 4), d = random(0.15, 0.25), color = self.trail_color})
+      self.area:addGameObject('TrailParticle',
+      self.x - 0.9 * self.w * math.cos(self.r) + 0.2 * self.w * math.cos(self.r + math.pi / 2), 
+      self.y - 0.9 * self.w * math.sin(self.r) + 0.2 * self.w * math.sin(self.r + math.pi / 2), 
+      {parent = self, r = random(2, 4), d = random(0.15, 0.25), color = self.trail_color})
+    end
   end)
+
+  if self.ship == 'Fighter' then
+    self.polygons[1] = {
+      self.w, 0,
+      self.w / 2, -self.h / 2,
+      -self.w / 2, -self.h / 2,
+      -self.w, 0,
+      -self.w / 2, self.h / 2,
+      self.w / 2, self.h / 2
+    }
+    self.polygons[2] = {
+      self.w / 2, -self.h / 2,
+      0, -self.h,
+      -self.w * 1.5, -self.h,
+      -self.w * 0.75, -self.h / 4,
+      -self.w / 2, -self.h / 2
+    }
+    self.polygons[3] = {
+      self.w / 2, self.h / 2,
+      0, self.h,
+      -self.w * 1.5, self.h,
+      -self.w * 0.75, self.h / 4,
+      -self.w / 2, self.h / 2
+    }
+  end
 end
 
 function Player:update(dt)
@@ -52,7 +86,22 @@ function Player:update(dt)
 end
 
 function Player:draw()
-  love.graphics.circle('line', self.x, self.y, self.w)
+  pushRotate(self.x, self.y, self.r)
+  love.graphics.setColor(default_color)
+  
+  -- draw polygons
+  for _, polygon in ipairs(self.polygons) do
+    local points = fn.map(polygon, function(v, k) 
+      if k % 2 == 1 then
+        return self.x + v + random(-1, 1)
+      else
+        return self.y + v + random(-1, 1)
+      end
+    end)
+    love.graphics.polygon('line', points)
+  end
+
+  love.graphics.pop()
 end
 
 function Player:shoot()
